2cbb3ae855e03c542e0198f69ba322bd61231bca
madhavpaysprint <madhavkumar@paysprint.in>
2024-12-05 16:58:42 +0530

    change message

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 |
  */
 $router->get('/', function () use ($router) {
-    return "Admin portal TEST Madhav this side and Hello JI";
+    return "Admin portal TEST";
 });
 $router->post('getrefid', 'ExampleController@QrHit');
 $router->post('cib-registration-status', 'BussinessBanking\PartnerRequests@getCibStatus');
